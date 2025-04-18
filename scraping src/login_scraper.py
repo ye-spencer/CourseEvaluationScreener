a18c9563ac6d236d0c6effd2cdb6a6c42588ce00
Spencer Ye <spencerye425@gmail.com>
2025-04-18 15:45:37 -0400

    Allow for multi-course download, created click-more

--- scraping src/login_scraper.py
+++ scraping src/login_scraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
-COURSES_TO_DOWNLOAD = ["functional programming", "software construction", "operating systems"]
+COURSES_TO_DOWNLOAD = ["functional programming", "operating systems"]
 
 ### Load environment variables ###
 load_dotenv()
@@ -23,7 +23,7 @@ sleep(3)
 ### Login to JHU SSO ###
 
 # Wait for and get username field
-WebDriverWait(browser, 10).until(lambda d : d.find_element(By.ID, 'i0116'))
+WebDriverWait(browser, 5).until(lambda d : d.find_element(By.ID, 'i0116'))
 username = browser.find_element(By.ID, 'i0116')
 username.send_keys(usernameStr)
 
@@ -32,25 +32,36 @@ nextButton = browser.find_element(By.ID, 'idSIButton9')
 nextButton.click()
 
 # Wait for and get password field
-WebDriverWait(browser, 20).until(lambda d : d.find_element(By.ID, 'i0118'))
+WebDriverWait(browser, 5).until(lambda d : d.find_element(By.ID, 'i0118'))
 password = browser.find_element(By.ID, 'i0118')
 password.send_keys(passwordStr)
 
 # Click submit button
-WebDriverWait(browser, 10).until(lambda d : d.find_element(By.ID, 'idA_PWD_ForgotPassword'))
+WebDriverWait(browser, 5).until(lambda d : d.find_element(By.ID, 'idA_PWD_ForgotPassword'))
 submitButton = browser.find_element(By.ID, "idSIButton9")
 submitButton.click()
 
 
-
+### Download PDFs for a given course ###
 def download_course(course_name):
     course_name = course_name.replace(" ", "+")
     link = f"https://asen-jhu.evaluationkit.com/Report/Public/Results?Course={course_name}&Instructor=&Search=true"
 
     browser.get(link)
 
-    sleep(10)
+    sleep(10) # WAIT: until the course search page is loaded
 
+    while True:
+        try:
+            WebDriverWait(browser, 5).until(lambda d : d.find_element(By.ID, 'publicMore')) # WAIT: until the show more button is loaded
+            show_more_button = browser.find_element(By.ID, 'publicMore')
+            show_more_button.click()
+        except:
+            break
+    print("Clicked show more as many times as possible")
+    sleep(10) # WAIT: until the the whole course search page is loaded
+
+    pdf_buttons = []
     pdf_buttons = browser.find_elements(By.CLASS_NAME, "sr-pdf")
     print(len(pdf_buttons))
     
@@ -70,13 +81,18 @@ def download_course(course_name):
 
     for url in URLS:
         browser.get(url)
-        sleep(10)
+        sleep(10) # WAIT: until the pdf is downloaded
 
+
+sleep(5) # WAIT: until the main page is loaded
+### Download PDFs for each course ###
 for course in COURSES_TO_DOWNLOAD:
     download_course(course)
 
 
 # TODO
-# ensure we can download all pdfs by clicking show more
 # remove sleeps and instead figure out how to wait properly with condition checks
 # - keep track of number of downloads and only stop once it matches the number of pdfs
+# - utilize 
+# error handling - if error, try again
+# move pdf downloads to a custom folder

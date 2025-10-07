95ed10ac70c1d6de3bdecd5ff4cb6332504ec774
Daniel Sung <daniel.sung.2005@gmail.com>
2025-10-06 20:48:32 -0400

    added thank you note

--- course-evaluation-fullstack/src/app/page.tsx
+++ course-evaluation-fullstack/src/app/page.tsx
@@ -1,7 +1,11 @@
+
+
 export default function Home() {
     return (
         <div>
             Welcome to our Course Evaluation Screener!
+            <p>Thank you for visiting!</p>
         </div>
+        
     );
 }
